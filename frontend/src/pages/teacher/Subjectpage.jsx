c19eb51feb10ff26d56cc7e75332f7646a5d076d
karkiraid35 <karkiraid35@gmail.com>
2024-06-18 19:03:42 +0100

    changing botton colors

--- frontend/src/pages/teacher/Subjectpage.jsx
+++ frontend/src/pages/teacher/Subjectpage.jsx
@@ -89,7 +89,7 @@ const ModuleEnvironmentPage = () => {
           <button onClick={DeployEnv} className="inline-flex bg-slate-400 items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-9 rounded-md px-3">
             Deploy 
           </button>
-          <button className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-9 rounded-md px-3">
+          <button className="inline-flex bg-slate-200 items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-9 rounded-md px-3">
             View environment
           </button>
         </div>
